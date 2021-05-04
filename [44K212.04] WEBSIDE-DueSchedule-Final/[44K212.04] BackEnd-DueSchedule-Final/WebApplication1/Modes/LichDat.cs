37fe21c65aaf74c8e10781cb3247a9e46987c27a
dinhhieuz <78039925+dinhhieuz@users.noreply.github.com>
2021-05-04 19:05:27 +0700

    Add files via upload

---
+++ [44K212.04] WEBSIDE-DueSchedule-Final/[44K212.04] BackEnd-DueSchedule-Final/WebApplication1/Modes/LichDat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Modes
+{
+    public class LichDat
+    {
+        //public string MaDat { get; set; }
+        public string NgayDat { get; set; }
+        public string KhungGio { get; set; }
+        //public string TrangThaiLV { get; set; }
+        //public int SoLuongSV { get; set; }
+        public string MaSV { get; set; }
+    }
+}
